5bb02e3c1be71205db19b549fed61fcb4b603fb5
ANDREW KRIEGER <ajkriegz@gmail.com>
2023-02-07 12:45:10 -0500

    continuing analysis and updating readme to clarify timeframe of data

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # COVID_SQL_analysis
 This analysis performed using Microsoft SQL Server Manager 19.0.1 and SQL Server 2019 Express. 
 
+Data is not current. Last date reported is 2021-04-30.
+
 ## Purpose 
 
 
--- queries.sql
+++ queries.sql
@@ -1,6 +1,11 @@
 SELECT *
 FROM PortfolioProject.dbo.CovidDeaths
+WHERE continent IS NOT NULL
 ORDER BY 3,4
+
+
+select MAX(date)
+FROM PortfolioProject.dbo.CovidDeaths
 
 
 --SELECT *
@@ -21,7 +26,24 @@ ORDER BY 1,2
 
 
 -- Looking at Total Cases vs Population
-SELECT location, date, population, total_cases, (total_cases/population)*100 AS PopulationInfected
+SELECT location, date, population, total_cases, (total_cases/population)*100 AS PercentPopulationInfected
 FROM PortfolioProject.dbo.CovidDeaths
 WHERE location like '%states%'
 ORDER BY 1,2
+
+
+-- Looing at countries with Highest Infection Rate vs Population
+SELECT location, population, MAX(total_cases) as HighestInfectionCount, MAX((total_cases/population))*100 AS PercentPopulationInfected
+FROM PortfolioProject.dbo.CovidDeaths
+-- WHERE location like '%states%'
+GROUP BY location, population
+ORDER BY PercentPopulationInfected DESC
+
+
+-- Showing countries with Highest Death Count per capita
+SELECT location, MAX(CAST(total_deaths AS INT)) AS TotalDeathCount
+FROM PortfolioProject.dbo.CovidDeaths
+WHERE continent IS NOT NULL
+GROUP BY location
+ORDER BY TotalDeathCount DESC
+
